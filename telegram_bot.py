2eb8ce2566eb58f9bfde484d162c15847b2f4b13
Volodymyr <vthehang@gmail.com>
2022-12-24 22:15:17 +0100

    added connection to postgres db

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,20 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from db_connection import DbConnection
 
 
 class TelegramBot:
     def __init__(self, tg_token: str) -> None:
-        self.app = ApplicationBuilder().token(tg_token).build()
+        self._app = ApplicationBuilder().token(tg_token).build()
+        self._db_connection = DbConnection().get_connection()
 
     async def _hello(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f'Hello {update.effective_user.full_name}')
 
     def run(self):
-        self.app.add_handler(CommandHandler('hello', self._hello))
+        self._app.add_handler(CommandHandler('hello', self._hello))
 
-        self.app.run_polling()
+        self._app.run_polling()
 
     def stop(self):
-        self.app.stop()
+        self._app.stop()
